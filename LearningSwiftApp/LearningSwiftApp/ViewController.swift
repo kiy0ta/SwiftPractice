1531aa7b984bfb0c2f7260e41a1cde26df8dd694
m.kiyota <m.kiyota@secure-i.jp>
2019-11-28 16:31:42 +0900

    create project

---
+++ LearningSwiftApp/LearningSwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningSwiftApp
+//
+//  Created by 清田武史 on 2019/11/28.
+//  Copyright © 2019 KIYOTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
